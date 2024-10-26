a9b61d8225771df9af7517e35457ecea513ddd9b
Teito <lasselaflair@gmail.com>
2024-10-26 22:57:39 +0200

    Refactor page component: Remove unused TestComp import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import TestComp from "@/components/TestComp";
 import ThemeSwitch from "@/components/Themes/ThemeSwitch";
 
 export default function Home() {
   return (
     <div>
       <ThemeSwitch />
-      <TestComp/>
     </div>
   );
 }
