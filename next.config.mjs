fb633d9e704a016835d01345b0edd6e522dbbc7c
Teito <lasselaflair@gmail.com>
2024-10-29 12:24:11 +0100

    Refactor styles: Update global styles and Sass options

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,8 @@
-import { stringify } from 'querystring';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    crossOrigin: 'anonymous',
-    async headers() {
-        return [
-            {
-                source: '/auth/:path*',
-                headers: [
-                    { key: 'Access-Control-Allow-Credentials', value: stringify(process.env.ACCESS_CONTROL_ALLOW_CREDENTIALS) },
-                    { key: 'Access-Control-Allow-Origin', value: stringify(process.env.ACCESS_CONTROL_ALLOW_ORIGIN) },
-                    { key: 'Access-Control-Allow-Methods', value: stringify(process.env.ACCESS_CONTROL_ALLOW_METHOD) },
-                    { key: 'Access-Control-Allow-Headers', value: stringify(process.env.ACCESS_CONTROL_ALLOW_HEADERS) },
-                    { key: "Referrer-Policy", value: "same-origin" }
-                ],
-            },
-        ];
-    },
     sassOptions: {
         // includePaths: ['styles'],
-        prependData: `@import "./src/styles/_colors";`
+        prependData: `@import "src/styles/colors"; @import "src/styles/fonts";`
     }
 };
 
